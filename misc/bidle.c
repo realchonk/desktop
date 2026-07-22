c4ba04fa9dcfc21078c14294e20e5e13663a921d
Benjamin Stürz <benni@stuerz.xyz>
2026-07-22 17:09:36 +0000

    bidle: rewrite as a portable xidle replacement with a manual
    Replace the non-functional event-driven bidle with a daemon that polls
    the MIT-SCREEN-SAVER extension (libXss) for the idle time and forks a
    program when it crosses a threshold -- the same approach xautolock
    takes, available on OpenBSD, FreeBSD, and Linux alike, unlike the
    OpenBSD-specific XIDLE extension the original xidle depends on.
    
    One trigger per idle episode (a flag cleared on observed activity) so a
    program that exits instantly cannot cause a respawn storm; the child is
    detached via setsid(2) and auto-reaped (SIGCHLD = SIG_IGN).  On OpenBSD,
    bidle pledges "stdio rpath proc exec" after opening the display.
    
    Options: -t timeout (def 300s), -p program (def slock), -i interval
    (def 1s), -n dry-run, -v verbose.  Add bidle.1 (mdoc) and wire it into
    misc/Mkfile's MANS.

--- misc/bidle.c
+++ misc/bidle.c
@@ -1,68 +1,171 @@
-#include <X11/extensions/scrnsaver.h>
+/*
+ * Copyright (c) 2025 Benjamin Stürz <benni@stuerz.xyz>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 #include <X11/Xlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <setjmp.h>
+#include <X11/extensions/scrnsaver.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <err.h>
 
-static Display *dpy;
-static int saver_event;
+static volatile sig_atomic_t got_signal;
 
-static void lock (void)
+static void
+sighandler (int sig)
+{
+	(void)sig;
+	got_signal = 1;
+}
+
+static int
+usage (void)
+{
+	fputs ("usage: bidle [-t timeout] [-p program] [-i interval] [-nv]\n",
+	       stderr);
+	return 1;
+}
+
+static void
+spawn (const char *prog, bool dry, bool verbose, unsigned long idle_ms)
 {
 	pid_t pid;
 
-	pid = fork ();
-	if (pid < 0)
-		err (1, "fork()");
+	if (dry) {
+		fprintf (stderr, "bidle: [dry-run] idle %lu ms, would run '%s'\n",
+			 idle_ms, prog);
+		return;
+	}
 
-	puts ("LOCKING SCREEN");
+	if (verbose)
+		fprintf (stderr, "bidle: idle %lu ms, running '%s'\n",
+			 idle_ms, prog);
+
+	pid = fork ();
+	if (pid < 0) {
+		warn ("fork()");
+		return;
+	}
 
 	if (pid == 0) {
 		setsid ();
-		execlp ("slock", "slock", NULL);
-		err (1, "exec()");
-	} else {
-		wait (NULL);
-		XSync (dpy, True);
+		execlp (prog, prog, (char *)NULL);
+		warn ("execlp('%s')", prog);
+		_exit (127);
 	}
 }
 
-int main (void)
+int
+main (int argc, char *argv[])
 {
-	XScreenSaverNotifyEvent *se;
-	XEvent ev;
-	int error, timeout;
+	const char *prog = "slock";
+	char *endp;
+	int timeout = 300, interval = 1, option;
+	bool dry = false, verbose = false, locked = false;
+	Display *dpy;
+	XScreenSaverInfo *info;
+	int evt, errb;
+	unsigned long timeout_ms, idle_ms;
+	Status st;
 
-	timeout = 5;
+	while ((option = getopt (argc, argv, "t:p:i:nv")) != -1) {
+		switch (option) {
+		case 't':
+			timeout = (int)strtol (optarg, &endp, 10);
+			if (*endp != '\0' || optarg[0] == '\0' || timeout <= 0)
+				errx (1, "invalid timeout: %s", optarg);
+			break;
+		case 'p':
+			prog = optarg;
+			break;
+		case 'i':
+			interval = (int)strtol (optarg, &endp, 10);
+			if (*endp != '\0' || optarg[0] == '\0' || interval <= 0)
+				errx (1, "invalid interval: %s", optarg);
+			break;
+		case 'n':
+			dry = true;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		default:
+			return usage ();
+		}
+	}
+
+	if (optind != argc)
+		return usage ();
+
+	signal (SIGCHLD, SIG_IGN);
+	signal (SIGINT, sighandler);
+	signal (SIGTERM, sighandler);
 
 	dpy = XOpenDisplay (NULL);
 	if (dpy == NULL)
 		errx (1, "cannot open display");
 
-	if (!XScreenSaverQueryExtension (dpy, &saver_event, &error))
-		errx (1, "no screen XScreenSaver extension");
+	if (XScreenSaverQueryExtension (dpy, &evt, &errb) == 0)
+		errx (1, "no MIT-SCREEN-SAVER extension on the server");
 
-	XScreenSaverSelectInput (dpy, DefaultRootWindow (dpy), ScreenSaverNotifyMask);
-	XSetScreenSaver (dpy, timeout, 0, DontPreferBlanking, DontAllowExposures);
-	
-	//signal (SIGINT, bye);
-	//signal (SIGTERM, bye);
+	info = XScreenSaverAllocInfo ();
+	if (info == NULL)
+		err (1, "XScreenSaverAllocInfo");
 
-	while (1) {
-		XNextEvent (dpy, &ev);
+#ifdef __OpenBSD__
+	if (pledge ("stdio rpath proc exec", NULL) == -1)
+		err (1, "pledge");
+#endif
 
-		if (ev.type == ClientMessage) {
-			XCloseDisplay (dpy);
-			return 0;
-		} else if (ev.type == saver_event) {
-			se = (XScreenSaverNotifyEvent *)&ev;
+	if (verbose)
+		fprintf (stderr, "bidle: timeout=%ds interval=%ds program='%s'\n",
+			 timeout, interval, prog);
 
-			if (se->forced == False)
-				lock ();
+	timeout_ms = (unsigned long)timeout * 1000UL;
+
+	for (;;) {
+		if (got_signal)
+			break;
+
+		st = XScreenSaverQueryInfo (dpy, DefaultRootWindow (dpy), info);
+		if (st == 0) {
+			warnx ("XScreenSaverQueryInfo failed, exiting");
+			break;
 		}
 
+		idle_ms = info->idle;
+
+		if (verbose)
+			fprintf (stderr, "bidle: idle=%lu ms%s\n", idle_ms,
+				 locked ? " (locked)" : "");
+
+		if (idle_ms >= timeout_ms) {
+			if (!locked) {
+				spawn (prog, dry, verbose, idle_ms);
+				locked = true;
+			}
+		} else {
+			locked = false;
+		}
+
+		XSync (dpy, False);
+		sleep (interval);
 	}
+
+	XFree (info);
+	XCloseDisplay (dpy);
+	return 0;
 }
